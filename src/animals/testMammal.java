93da4bb94500b39d1783622c73e353d7919daeeb
xvxiaopei <xvxiaopeisjtu@gmail.com>
2017-04-19 17:40:45 -0500

    Create

---
+++ src/animals/testMammal.java
@@ -0,0 +1,18 @@
+package animals;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testMammal {
+
+	@Test
+	public void test() {
+		assertEquals(1,1);
+	}
+	@Test
+	public void test1(){
+		assertEquals(1,1);
+	}
+
+}
